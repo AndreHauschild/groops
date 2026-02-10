9e8db1f9b6cb31568bd62080f7f156df662f6a9f
AndreHauschild <andre.hauschild@gmx.de>
2026-02-10 13:48:58 +0100

    Update comment and documentation

--- source/programs/simulation/simulateStarCameraMetopSG.cpp
+++ source/programs/simulation/simulateStarCameraMetopSG.cpp
@@ -2,10 +2,10 @@
 /**
 * @file simulateStarCameraMetopSG.cpp
 *
-* @brief Rotation from satellite (x: along, y: cross, z: nadir) to inertial frame.
+* @brief Simulate star camera data for Metop-SG.
 *
-* @author Torsten Mayer-Guerr
-* @date 2005-01-21
+* @author Andre Hauschild
+* @date 2026-02-10
 */
 /***********************************************/
 
@@ -13,11 +13,9 @@
 #define DOCSTRING docstring
 static const char *docstring = R"(
 This program simulates \file{star camera}{instrument} measurements at each satellite's position.
-The satellite's orientation follows a local orbit frame with the x-axis in along track (along velocity),
-y-axis is cross track (normal to position and velocity vector) and z-axis pointing nadir (negative position vector).
-As for non circular orbit the position and velocity are not exact normal, the default is the x-axis to be exact
-along velocity and the z-axis forms a right hand system (not exact nadir) or with \config{nadirPointing} the z-axis
-is exact nadir and x-axis approximates along.
+The satellite's orientation follows a local orbit frame with the z-axis pointing downwards normal 
+to the Earth's ellipsoid, the y-axis perpendicular to the Earth-fixed velocity and the x-axis 
+oriented in the direction of the Earth-fixed velocity.
 The resulting rotation matrices rotate from satellite frame to inertial frame.
 )";
 
@@ -29,7 +27,7 @@ The resulting rotation matrices rotate from satellite frame to inertial frame.
 
 /***** CLASS ***********************************/
 
-/** @brief Rotation from satellite (x: along, y: cross, z: nadir) to inertial frame.
+/** @brief Simulate star camera data for Metop-SG.
 * @ingroup programsGroup */
 class SimulateStarCameraMetopSG
 {
@@ -37,7 +35,7 @@ public:
   void run(Config &config, Parallel::CommunicatorPtr comm);
 };
 
-GROOPS_REGISTER_PROGRAM(SimulateStarCameraMetopSG, PARALLEL, "Rotation from satellite (x: along, y: cross, z: nadir) to inertial frame.", Simulation, Instrument)
+GROOPS_REGISTER_PROGRAM(SimulateStarCameraMetopSG, PARALLEL, "Simulate star camera data for Metop-SG.", Simulation, Instrument)
 
 /***********************************************/
 
@@ -49,10 +47,9 @@ void SimulateStarCameraMetopSG::run(Config &config, Parallel::CommunicatorPtr co
     EarthRotationPtr earthRotation;
     Bool     isNadirPointing;
 
-    readConfig(config, "outputfileStarCamera", starCameraName,  Config::MUSTSET, "",  "rotation from satellite to inertial frame (x: along, y: cross, z: nadir)");
+    readConfig(config, "outputfileStarCamera", starCameraName,  Config::MUSTSET, "",  "rotation from satellite to inertial frame (x: along Earth-fixed velocity, y: cross, z: normal to Earth ellipsoid)");
     readConfig(config, "inputfileOrbit",       orbitName,       Config::MUSTSET, "",  "position and velocity defines the orientation of the satellite at each epoch");
     readConfig(config, "earthRotation",        earthRotation,   Config::MUSTSET,  "", "transformation from CRF to TRF");
-    readConfig(config, "nadirPointing",        isNadirPointing, Config::DEFAULT, "1", "false: exact along and nearly nadir, true: nearly along and exact nadir");
 
     if(isCreateSchema(config)) return;
 
@@ -68,7 +65,7 @@ void SimulateStarCameraMetopSG::run(Config &config, Parallel::CommunicatorPtr co
       {
 
         // CRF position and velocity
-
+        Vector3d position = orbit.at(i).position;
         Vector3d velocity = orbit.at(i).velocity; // velocity vector
         if(velocity.r()==0)
         {
@@ -79,21 +76,21 @@ void SimulateStarCameraMetopSG::run(Config &config, Parallel::CommunicatorPtr co
         }
 
         // CRF to TRF rotation
-
         const Rotary3d rot = earthRotation->rotaryMatrix(orbit.at(i).time);
         const Vector3d omega = rot.rotate(earthRotation->rotaryAxis(orbit.at(i).time));
 
         // TRF position and velocity
-
-        const Vector3d position = rot.rotate(orbit.at(i).position);
+        position = rot.rotate(orbit.at(i).position);
         velocity = rot.rotate(orbit.at(i).velocity) - crossProduct(omega, position);
 
+        position = localNorthEastUp(position, Ellipsoid()).transform(Vector3d(0,0,1)); // up component
+
         Vector3d y = normalize(crossProduct(velocity, position)); // cross
-        Vector3d x = normalize((isNadirPointing) ? crossProduct(position, y) : velocity);  // along
+        Vector3d x = normalize(crossProduct(position, y));        // along
 
         StarCameraEpoch epoch;
         epoch.time   = orbit.at(i).time;
-        epoch.rotary = inverse(rot)*Rotary3d(x, y); // rotation crf <- trf + trf <- sat
+        epoch.rotary = inverse(rot)*Rotary3d(x, y); // rotation crf <- trf * trf <- sat
         arc.push_back(epoch);
       }
       return arc;
