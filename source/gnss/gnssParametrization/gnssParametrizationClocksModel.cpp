54c8f23604272b0b9f2417587432035e40d8ac96
AndreHauschild <andre.hauschild@gmx.de>
2026-02-25 10:49:32 +0100

    Swap order of transmitter and receiver in designMatrixIsl

--- source/gnss/gnssParametrization/gnssParametrizationClocksModel.cpp
+++ source/gnss/gnssParametrization/gnssParametrizationClocksModel.cpp
@@ -282,13 +282,13 @@ void GnssParametrizationClocksModel::designMatrixIsl(const GnssNormalEquationInf
 {
   try
   {
-    // receiver clock
-    if(indexTrans.at(eqn.receiver->idTrans()).size() && indexTrans.at(eqn.receiver->idTrans()).at(eqn.idEpoch))
-      copy(eqn.A.column(GnssObservationEquationIsl::idxClockRecv,1), A.column(indexTrans.at(eqn.receiver->idTrans()).at(eqn.idEpoch)));
-
     // transmitter clock
     if(indexTrans.at(eqn.transmitter->idTrans()).size() && indexTrans.at(eqn.transmitter->idTrans()).at(eqn.idEpoch))
       copy(eqn.A.column(GnssObservationEquationIsl::idxClockTrans,1), A.column(indexTrans.at(eqn.transmitter->idTrans()).at(eqn.idEpoch)));
+
+    // receiver clock
+    if(indexTrans.at(eqn.receiver->idTrans()).size() && indexTrans.at(eqn.receiver->idTrans()).at(eqn.idEpoch))
+      copy(eqn.A.column(GnssObservationEquationIsl::idxClockRecv,1), A.column(indexTrans.at(eqn.receiver->idTrans()).at(eqn.idEpoch)));
   }
   catch(std::exception &e)
   {
