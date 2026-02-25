5b8a70265e4734f95b1847e135646673948e9a0c
AndreHauschild <andre.hauschild@gmx.de>
2026-02-25 13:01:34 +0100

    Revert separation of ACV and bias corrections for GNSS observations

--- source/gnss/gnssTransceiver.h
+++ source/gnss/gnssTransceiver.h
@@ -68,10 +68,6 @@ public:
   /** @brief Allowed signal types. Empty if no  definition was provided. */
   std::vector<GnssType> definedTypes(const Time &time) const;
 
-  /** @brief Signal bias corrections.
-  * observed range = range + bias. */
-  Vector signalBiases(const std::vector<GnssType> &type) const;
-
   /** @brief Direction dependent corrections.
   * observed range = range (ARPs of transmitter and receiver) + antennaVariations. */
   Vector antennaVariations(const Time &time, Angle azimut, Angle elevation, const std::vector<GnssType> &type) const;
@@ -146,30 +142,21 @@ inline std::vector<GnssType> GnssTransceiver::definedTypes(const Time &time) con
 
 /***********************************************/
 
-inline Vector GnssTransceiver::signalBiases(const std::vector<GnssType> &types) const
-{
-  try
-  {
-    return signalBias.compute(types);
-  }
-  catch(std::exception &e)
-  {
-    GROOPS_RETHROW(e)
-  }
-}
-
-/***********************************************/
-
 inline Vector GnssTransceiver::antennaVariations(const Time &time, Angle azimut, Angle elevation, const std::vector<GnssType> &types) const
 {
   try
   {
+    Vector corr(types.size());
+    corr += signalBias.compute(types);
+
     auto antenna = platform.findEquipment<PlatformGnssAntenna>(time);
     if(!antenna)
       throw(Exception(platform.markerName+"."+platform.markerNumber+": no antenna definition found at "+time.dateTimeStr()));
     if(!antenna->antennaDef)
       throw(Exception("no antenna definition for "+antenna->str()));
-    return antenna->antennaDef->antennaVariations(azimut, elevation, types, noPatternFoundAction);
+    corr += antenna->antennaDef->antennaVariations(azimut, elevation, types, noPatternFoundAction);
+
+    return corr;
   }
   catch(std::exception &e)
   {
